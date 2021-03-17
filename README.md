65c6b8e502b20020a123a284b22dae7e90fb38fb
orianemgn <mgnoriane@gmail.com>
2021-03-17 15:21:05 +0100

    info

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Morning-Kata
+After the bootcamp we decided after several junior web developers to create a morning kata club. 
+The Kata here were solved in groups or alone. 
--- TGIFriday.js
+++ TGIFriday.js
@@ -1,4 +1,4 @@
-// TGI Friday!!
+// TGI Friday!! (solved in group)
 // We all love fridays, and even better if it is the last day of the month!
 // In this kata you should write a function that will receive 2 parameters. Both are years, and indicates a range.
 // Your work is to return the number of times a month ends with a Friday.
