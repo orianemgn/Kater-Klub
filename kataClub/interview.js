e48337f331b66468d062c4d11a41766945d1588b
orianemgn <mgnoriane@gmail.com>
2021-03-25 11:45:28 +0100

    kater klub

--- kataClub/interview.js
+++ kataClub/interview.js
@@ -1,6 +1,8 @@
 // Given a string of characters find the first character that occurs only once,
 //  e.g., in `supercalifragilisticexpialidocious` it is `f`
 
+
+//My solution 
 function FirstUniqueCharacter(s){
     //you iterate over the string
     for (let i = 0; i<s.length; i++){
@@ -13,4 +15,26 @@ function FirstUniqueCharacter(s){
         return j; 
       }
     }  
+  }
+
+
+  //Solution Hai 
+  function findCharacter(str) {
+    const charOccObj = str.split("").reduce((obj, char) => {
+      obj[char] = obj[char] === undefined ? 1 : obj[char] + 1;
+      return obj;
+    }, {})
+    for (let key in charOccObj) {
+      if (charOccObj[key] === 1) return key;
+    }
+  }
+
+  //Solution Michael 
+  function firstOccurance(s) {
+    const compareObj = {};
+    for(i=0; i < s.length; i++) {
+      (compareObj[s[i]] === undefined) ? compareObj[s[i]] = 1 : compareObj[s[i]]++;
+    }
+    const firstIndex = Object.values(compareObj).indexOf(1);
+    return Object.keys(compareObj)[firstIndex]
   }
