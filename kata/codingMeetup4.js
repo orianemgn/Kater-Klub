938d6a44ce753d3c57f0ff195435280e9d489b0f
orianemgn <mgnoriane@gmail.com>
2021-06-23 16:39:13 +0200

    coding meetup 4 - train to work with object

---
+++ kata/codingMeetup4.js
@@ -0,0 +1,19 @@
+//Coding Meetup #4 - Higher-Order Functions Series - Find the first Python developer
+// https://www.codewars.com/kata/5827bc50f524dd029d0005f2/train/javascript 
+
+function getFirstPython(list) {
+    // Thank you for checking out my kata :)
+    //iterate in the object
+    // if object includes 'Python' return first name 
+    // if not return other sentence
+    let pythonDeveloperName = []; 
+    let pythonDeveloperCountry = [];
+    for(const[key,value] of Object.entries(list)){
+      console.log(value.language, )
+      if(value.language === 'Python'){
+        pythonDeveloperName.push(value.firstName)
+        pythonDeveloperCountry.push(value.country)
+      }
+    }
+    return pythonDeveloperName.length === 0 ? 'There will be no Python developers': `${pythonDeveloperName[0]}, ${pythonDeveloperCountry[0]}`
+  }
