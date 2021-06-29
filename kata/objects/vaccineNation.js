f3e2775a3f588c5e13aca0ac059237f234a29d24
orianemgn <mgnoriane@gmail.com>
2021-06-29 22:57:57 +0200

    added explenation to my solution

--- kata/objects/vaccineNation.js
+++ kata/objects/vaccineNation.js
@@ -3,6 +3,7 @@
 
 
 function vaccineList(age, status, month) {
+    // I create an object with the datas 
     let vaccine = {
       fiveInOne : ['8 weeks', '12 weeks', '16 weeks'],
       pneumococcal : ['8 weeks', '16 weeks'],
@@ -13,16 +14,23 @@ function vaccineList(age, status, month) {
       'offer fluVaccine' : ['september','october','november'],
       preSchoolBooster : ['40 months']
     }
+    // I create an empty array to push the result 
       let result = []
+      //I iterate inside my object
     for(let[key,value] of Object.entries(vaccine)){
+        // I iterate inside the array values of the object 
       for(let i=0; i<value.length; i++){
+          // if one of the value in the array is equal to the paramether then push the key in the result array
         if(age === (value[i]) || status === (value[i]) || month ===(value[i])){
           result.push(key)
         }
       }
     }
-  
+
+
+  // I delete the duplicates
     let uniqueResult = [...new Set(result)]; 
+  // I sort the array 
     let sortedResult = uniqueResult.sort(); 
     
     return sortedResult
